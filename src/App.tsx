063e746abbda029324cec4508a77a98b7a4fbab9
leoporto166 <leonardo.porto166@gmail.com>
2026-01-12 20:16:06 -0300

    Parte Projeto e Pop-up

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,31 @@ import curriculo from "../src/assets/Leonardo Porto.pdf"
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import siteGec from "./assets/site_gec.png"
+import { useState } from 'react';
+import videoGO from "./assets/Vido_site_go.mp4"
+import { FiX } from "react-icons/fi";
+
 function App() {
+
+  const [open, setOpen] = useState(false)
 
 
   return (
     <>
-      <main className='Body h-screen'>
+      <main className='Body '>
         <section className=''>
 
         </section>
 
-        <section className='flex justify-between h-75 items-center  p-2'>
-          <div>
+        <section className='flex justify-between p-2'>
+          <div className='flex flex-col justify-center'>
             <h1 className='text-white text-5xl font-extrabold'>Olá, eu sou o <br /> <p className='text-green-400 text-shadow-lg text-shadow-white/10 mb-4 '>Leonardo Porto</p></h1>
 
 
             <a href={curriculo}
             download
-            className='text-white bg-green-600 p-2 rounded-2xl hover:bg-white transition-all duration-600 hover:text-green-600'
+            className='text-white bg-green-600 p-2 rounded-2xl hover:bg-white transition-all duration-600 hover:text-green-600 w-23 text-center'
             >
               Baixar CV
             </a>
@@ -38,7 +45,7 @@ function App() {
 
         <section>
 
-          <div className='p-2'>
+          <div className='p-2 flex flex-col '>
             <h2 className='text-green-400 text-3xl font-extrabold'>Resumo Pessoal</h2>
 
             <p className='text-white'>
@@ -62,23 +69,79 @@ function App() {
             <h2 className='text-green-400 text-3xl font-extrabold'>Meus Projetos</h2>
           </div>
 
-          <Swiper
-            modules={[Navigation, Pagination, Autoplay]}
-                slidesPerView={3}
-                spaceBetween={0}
-                navigation
-                autoplay = {{delay: 500}}
-                breakpoints={{}}
-          >
-            <SwiperSlide>
-              <img src={foto} className='w-50'></img>
-            </SwiperSlide>
 
-            <SwiperSlide>
-              <img src={foto} className='w-50'></img>
-            </SwiperSlide>
-            
-          </Swiper>
+
+          <section className='flex p-2 '>
+
+            <div className='border-2 border-green-400 rounded-lg w-110 h-90 mr-2 flex flex-col items-center shadow-lg shadow-white/8'>
+
+              <div className='w-[360px] flex my-2'>
+
+                <h1 className='text-white text-2xl font-bold'>Projeto Goias Esporte Clube</h1>
+
+              </div>
+              <img src={siteGec} alt="Imagem site Goias" className='w-90 h-55 mt-2' />
+
+              <div className='w-[360px] flex mt-2'>
+                <button
+                onClick={() => setOpen(true)}
+                >
+                <p className='bg-green-300 p-1 rounded-xl font-medium cursor-pointer hover:bg-amber-50/0 
+                border border-green-300 hover:text-white transition-all duration-300'>Abrir previa</p>
+                </button>
+              </div>
+
+              
+          {open && (
+            <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+              <div className="bg-white p-6 rounded-lg w-200 shadow-lg h-200 justify-center items-center flex flex-col">
+                <div className='w-full justify-end flex mb-2'>
+                  <button onClick={() => setOpen(false)}>
+                  <FiX size={25} className='cursor-pointer'></FiX>
+                  
+                                 </button>
+                </div>
+
+               <video className='w-130 mb-2'
+               autoPlay 
+               loop 
+               muted
+               controls={false}
+               >
+                <source  src={videoGO} type='video/mp4'/>
+                 Seu navegador não suporta o video.
+               </video>
+
+               <p>
+                Desenvolvimento de um site institucional moderno e responsivo, utilizando React como framework principal. O projeto conta com layout personalizado, identidade visual alinhada ao clube e foco em experiência do usuário.
+
+
+                <br />
+                <br />
+
+                Foram implementados menus responsivos, carrosséis dinâmicos de notícias e jogos com Swiper, além da organização de componentes reutilizáveis. O projeto também utiliza Tailwind CSS para uma estilização eficiente e consistente, garantindo boa performance e adaptação a diferentes tamanhos de tela.
+
+                <br />
+                <br />
+
+                Além disso, o sistema conta com autenticação de usuários, incluindo login e senha, controle de acesso e proteção de rotas, assegurando que determinadas áreas da aplicação sejam acessadas apenas por usuários autenticados.
+
+                <br />
+                <br />
+
+                O objetivo foi simular um portal esportivo profissional, destacando conteúdos relevantes como notícias, partidas e informações do clube, aplicando boas práticas de desenvolvimento front-end, organização de código e conceitos básicos de segurança na aplicação.
+               </p>
+              </div>
+            </div>
+          )}
+
+          
+            </div>   
+
+
+          </section>
+
+  
 
         </section>
       </main>
